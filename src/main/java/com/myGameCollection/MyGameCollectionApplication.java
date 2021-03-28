6ccb4fa329300e5ed3e6a6c0e391f5bd1b06d08a
Ricardo <ricardo.abarza.v@zoho.com>
2021-03-28 11:36:45 -0300

    first commit

---
+++ src/main/java/com/myGameCollection/MyGameCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.myGameCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myGameCollection/MyGameCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myGameCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
